25bf7ee3e1941cf6a8f647f3c8b27466a4bd155a
Allassane <dickoallassane1997@gmail.com>
2022-12-07 12:48:44 +0100

    Remove old commented code

--- src/index.js
+++ src/index.js
@@ -2,39 +2,7 @@ import './style.css';
 import ListTasks from './modules/ListTasks.js';
 
 /* List Task */
-const listTask = new ListTasks(); // [new Task('Task 1', false)];
-/* Sort List Of task */
-
-/* Display List */
-/*
-listTask.forEach((task) => {
-  const ulListTask = document.getElementById('list-task');
-  const liTask = document.createElement('li');
-  liTask.classList.add('task');
-
-  const divTaskContent = document.createElement('div');
-  divTaskContent.classList.add('task-content');
-  const checkBok = document.createElement('input');
-  checkBok.type = 'checkbox';
-
-  const inTaskDescription = document.createElement('input');
-  inTaskDescription.classList.add('task-description');
-  inTaskDescription.value = task.description;
-  inTaskDescription.type = 'text';
-
-  /* Display element */
-/* divTaskContent.append(checkBok);
-  divTaskContent.append(inTaskDescription);
-
-  const iMenu = document.createElement('i');
-  iMenu.classList.add('fa-solid', 'fa-ellipsis-vertical');
-
-  liTask.append(divTaskContent);
-  liTask.append(iMenu);
-
-  ulListTask.append(liTask);
-});
-*/
+const listTask = new ListTasks();
 
 /* On submit the input */
 const form = document.forms[0];
